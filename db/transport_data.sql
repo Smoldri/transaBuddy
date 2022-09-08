607e73fae5fb5131d2ec741c6ee55eb27aeac0c5
Siim <moldri.siim@gmail.com>
2022-09-08 15:12:16 +0300

    SIIMM initial commit

---
+++ db/transport_data.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.contact (id, first_name, last_name, personal_code, phone_number, email) VALUES (DEFAULT, 'Mai', 'Legaalne', '40000000001', '1234', 'moldrdasdasi@com');
+INSERT INTO public.contact (id, first_name, last_name, personal_code, phone_number, email) VALUES (DEFAULT, 'Siim', 'Massiiv', '40000000002', '4566', 'moldrdasdasi@com');
+INSERT INTO public.contact (id, first_name, last_name, personal_code, phone_number, email) VALUES (DEFAULT, 'Siim', 'James Bond', '40000000003', '6775', 'moldrdasdasi@com');
+INSERT INTO public.contact (id, first_name, last_name, personal_code, phone_number, email) VALUES (DEFAULT, 'Eno', 'Raud', '30000000004', '87686', 'moldrdasdasi@com');
