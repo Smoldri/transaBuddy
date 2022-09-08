c62f2a3d2a0537a1b6b4103ad88e2bfb3ffa7c1d
Siim <moldri.siim@gmail.com>
2022-09-08 13:50:41 +0300

    Initial commit

---
+++ src/test/java/com/example/transaBuddy/TransaBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.transaBuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransaBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
