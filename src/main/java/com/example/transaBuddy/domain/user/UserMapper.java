62ab5be38d21b3eca6eca65c31bd23debd2f4170
Siim <moldri.siim@gmail.com>
2022-09-13 10:58:43 +0300

    SIIMM

--- src/main/java/com/example/transaBuddy/domain/user/UserMapper.java
+++ src/main/java/com/example/transaBuddy/domain/user/UserMapper.java
@@ -11,11 +11,13 @@ import org.mapstruct.ReportingPolicy;
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = "spring")
 public interface UserMapper {
     @Mapping(target = "userId", source = "id" )
+
     UserResponse userToUserResponse (User user);
     User userRequestToUser(UserRequest userRequest);
     @Mapping(target = "userId", source = "id")
     @Mapping(target = "firstName", source = "contact.firstName")
     @Mapping(target = "lastName", source = "contact.lastName")
+    @Mapping(target = "contactId", source = "contact.id")
     @Mapping(target = "personalCode", source = "contact.personalCode")
     @Mapping(target = "email", source = "contact.email")
     @Mapping(target = "phoneNumber", source = "contact.phoneNumber")
