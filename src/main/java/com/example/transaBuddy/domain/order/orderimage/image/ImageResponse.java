4ebb29b454ce2721308c2a3fde395a1f0bb80d69
Siim <moldri.siim@gmail.com>
2022-09-15 12:43:55 +0300

    SIIMM_TASK4- get images bt order id

---
+++ src/main/java/com/example/transaBuddy/domain/order/orderimage/image/ImageResponse.java
@@ -0,0 +1,12 @@
+package com.example.transaBuddy.domain.order.orderimage.image;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ImageResponse {
+    private String base64;
+}
