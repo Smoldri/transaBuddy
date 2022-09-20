b897bf1a76fa70dc8307c6af32ef355d9b0762aa
klves <95514872+klves@users.noreply.github.com>
2022-09-20 13:12:18 +0300

    KAUR - updated OrderInfo with variables

--- src/main/java/com/example/transaBuddy/domain/order/OrderService.java
+++ src/main/java/com/example/transaBuddy/domain/order/OrderService.java
@@ -124,18 +124,18 @@ public class OrderService {
         return activeOrders;
     }
 
-        public List<OrderInfo> findAllOrdersByDistrictAndPickUpDropOffType (Integer districtId, String pickUpDropOffType){
-            List<PickUpDropOff> pickUpsDropOffs = pickUpDropOffRepository.findByDistrictIdAndType(districtId, pickUpDropOffType);
-            List<Order> orders = new ArrayList<>();
-            for (PickUpDropOff pickUpDropOff : pickUpsDropOffs) {
-                orders.add(pickUpDropOff.getOrder());
-            }
-            List<OrderInfo> orderInfos = orderMapper.ordersToOrderInfos(orders);
-            for (OrderInfo orderInfo : orderInfos) {
-                addLocationsToOrderInfo(orderInfo);
-            }
-            return orderInfos;
-        }
+//        public List<OrderInfo> findAllOrdersByDistrictAndPickUpDropOffType (Integer districtId, String pickUpDropOffType){
+//            List<PickUpDropOff> pickUpsDropOffs = pickUpDropOffRepository.findByDistrictIdAndType(districtId, pickUpDropOffType);
+//            List<Order> orders = new ArrayList<>();
+//            for (PickUpDropOff pickUpDropOff : pickUpsDropOffs) {
+//                orders.add(pickUpDropOff.getOrder());
+//            }
+//            List<OrderInfo> orderInfos = orderMapper.ordersToOrderInfos(orders);
+//            for (OrderInfo orderInfo : orderInfos) {
+//                addLocationsToOrderInfo(orderInfo);
+//            }
+//            return orderInfos;
+//        }
 
         public List<OrderInfo> findAllOrdersByPickUpAndOrDropOffDistrict (Integer pickUpDistrictId, Integer
         dropOffDistrictId){
